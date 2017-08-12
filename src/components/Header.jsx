4d10e54a51074b09e99d1a3120dbf4dcb6c613ca
mlkiung <meredith.kiung@gmail.com>
2017-08-12 12:14:35 -0400

    search onChange working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
 class Header extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      searchTerm: ''
+    }
+
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onSearchSubmit = this.onSearchSubmit.bind(this)
+  }
+
+  onSearchChange(event) {
+    event.preventDefault()
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  onSearchSubmit(event) {
+    event.preventDefault()
+  }
+
   render() {
     return (
       <div className="header">
@@ -14,8 +36,12 @@ class Header extends Component {
               <input
                 className="u-full-width"
                 type="text"
-                placeholder="Puppies . . ."
-                id="search-flickr" />
+                placeholder="Puppies?"
+                id="search-flickr"
+                value={this.state.searchTerm}
+                onChange={this.onSearchChange}
+                onSubmit={this.onSearchSubmit}
+              />
             </div>
           </div>
         </div>
